d291a6a56c927ea6ebc9916f9cb3fd2260879793
OumaimaEllaouzy <ellaouzyoumaima@gmail.com>
2024-05-11 19:56:57 +0200

    modification lignedevente controller lignedevente

--- src/main/java/com/example/salesmaster/Controllers/LigneDeVenteController.java
+++ src/main/java/com/example/salesmaster/Controllers/LigneDeVenteController.java
@@ -1,5 +1,7 @@
 package com.example.salesmaster.Controllers;
 import com.example.salesmaster.entities.LigneDeVente;
+import com.example.salesmaster.entities.LigneDeVenteKey;
+import com.example.salesmaster.services.LigneDeVenteService;
 import com.example.salesmaster.services.LigneDeVenteServiceimpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,40 +16,43 @@ import java.util.List;
 @RestController
 public class LigneDeVenteController {
     @Autowired
-    private LigneDeVenteServiceimpl ligneDeVenteService;
-
+    private LigneDeVenteService ligneDeVenteService;
 
     @GetMapping
     public ResponseEntity<List<LigneDeVente>> getLigneDeVentes()
     {
-        return ResponseEntity.ok(ligneDeVenteService.getAllLigneDeVente());
+        List<LigneDeVente> allLignedeVente = ligneDeVenteService.getAllLigneDeVente();
+        return new ResponseEntity<>(allLignedeVente, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<LigneDeVente> getLigneDeVente(@PathVariable("id") Long ligneDeVenteId)
-    {
-        return ResponseEntity.ok(ligneDeVenteService.getLigneDeVenteById(ligneDeVenteId));
+
+    @GetMapping("/{venteId}/{produitId}")
+
+    public ResponseEntity<LigneDeVente> getLigneDeVenteById(@PathVariable Long venteId, @PathVariable Long produitId) {
+        LigneDeVenteKey ligneDeVenteId = new LigneDeVenteKey(venteId, produitId);
+        LigneDeVente ligneDeVente = ligneDeVenteService.getLigneDeVenteById(ligneDeVenteId);
+        return new ResponseEntity<>(ligneDeVente, HttpStatus.OK);
     }
+
 
     @PostMapping
-    public ResponseEntity<LigneDeVente> createLigneDeVente(@RequestBody LigneDeVente ligneDeVente)
-    {
-        LigneDeVente ligneDeVente1 = ligneDeVenteService.saveLigneDeVente(ligneDeVente);
-        return new ResponseEntity<>(ligneDeVente1, HttpStatus.CREATED);
+    public ResponseEntity<LigneDeVente> saveLigneDeVente(@RequestBody LigneDeVente ligneDeVente) {
+        System.out.println("Id of vente&produit : " + ligneDeVente.getId().getVenteId()+" "+ligneDeVente.getId().getProduitId() + "quantity : " + ligneDeVente.getQuantity() + "Prix Unitaire : " + ligneDeVente.getPrixUnitaire());
+        LigneDeVente createdLignedeVente = ligneDeVenteService.saveLigneDeVente(ligneDeVente);
+        return new ResponseEntity<>(createdLignedeVente, HttpStatus.CREATED);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<LigneDeVente> updateLigneDeVente(@PathVariable("id") Long ligneDeVenteId , @RequestBody LigneDeVente ligneDeVente)
-    {
-        LigneDeVente lignedeVente1 = ligneDeVenteService.updateLigneDeVente(ligneDeVenteId,ligneDeVente);
-        return new ResponseEntity<>(lignedeVente1, HttpStatus.CREATED);
-    }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteLigneDeVente(@PathVariable("id") Long ligneDeVenteId)
-    {
-        ligneDeVenteService.deleteLigneDeVenteById(ligneDeVenteId);
-        return ResponseEntity.ok("LigneDeVente deleted successfully !");
-    }
+    @PutMapping("/{venteId}/{produitId}")
+    public ResponseEntity<LigneDeVente> updateLigneDeVente(@PathVariable Long venteId, @PathVariable Long produitId, @RequestBody LigneDeVente ligneDeVenteDto) {
+        LigneDeVenteKey ligneDeVenteId = new LigneDeVenteKey(venteId, produitId);
+        LigneDeVente updatedLignedeVente = ligneDeVenteService.updateLigneDeVente(ligneDeVenteId, ligneDeVente);
+        return new ResponseEntity<>(updatedLignedeVente, HttpStatus.OK);
 
+    @DeleteMapping("/{venteId}/{produitId}")
+    public ResponseEntity<Void> deleteLigneDeVente(@PathVariable Long venteId, @PathVariable Long produitId) {
+        LigneDeVenteKey lignedeVenteId = new LigneDeVenteKey(venteId, produitId);
+        ligneDeVenteService.deleteLigneDeVente(lignedeVenteId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
